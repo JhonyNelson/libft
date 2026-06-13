773dc8e1bf0d98c4a96835979d66abefa8c70582
Jhony Nelson <jhony.nelson@hotmail.com>
2026-06-13 16:45:33 -0300

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,12 +12,38 @@ static size_t	count_digits(long n)
 	while (n > 0)
 	{
 		count++;
-		n /= 10;
+		n = n / 10;
 	}
 	return (count);
 }
 
 char	*ft_itoa(int n)
 {
+	char	*str;
+	long	nb;
+	size_t	len;
 
+	nb = n;
+	len = count_digits(nb);
+	if (nb < 0)
+		len++;
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	if (nb == 0)
+		str[0] = '0';
+	while (nb > 0)
+	{
+		str[--len] = (nb % 10) + '0';
+		nb = nb / 10;
+	}
+	return (str);
 }
+// [0] [1] [2] [3]
+//  -   4   2  \0
