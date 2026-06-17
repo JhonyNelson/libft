b26ac60aeb252c40ebf9366cdc4ad17beaf10a6a
Jhony Nelson <jhony.nelson@hotmail.com>
2026-06-17 01:31:39 -0300

    terminando

---
+++ ft_lstmap.c
@@ -0,0 +1,32 @@
+#include "libft.h"
+
+static t_list	*map_error(t_list **lst, void (*del)(void *), void *content)
+{
+	ft_lstclear(lst, del);
+	if (content)
+		del(content);
+	return (NULL);
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*new_node;
+	void	*new_content;
+
+	if (!f || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		new_content = f(lst->content);
+		if (!new_content)
+			return (map_error(&new_list, del, NULL));
+		new_node = ft_lstnew(new_content);
+		if (!new_node)
+			return (map_error(&new_list, del, new_content));
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
---
+++ ft_split.c
@@ -0,0 +1,92 @@
+#include "libft.h"
+
+static size_t	count_words(char const *s, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+			count++;
+		while (s[i] && s[i] != c)
+			i++;
+	}
+	return (count);
+}
+
+static void	free_split(char **split, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size)
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+static char	*get_word(char const *s, size_t start, size_t len)
+{
+	char	*word;
+	size_t	i;
+
+	word = malloc((len + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = s[start + i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static int	fill_split(char **split, char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	size_t	start;
+
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+		{
+			split[j] = get_word(s, start, i - start);
+			if (!split[j])
+				return (free_split(split, j), 0);
+			j++;
+		}
+	}
+	split[j] = NULL;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+
+	if (!s)
+		return (NULL);
+	split = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!split)
+		return (NULL);
+	if (!fill_split(split, s, c))
+		return (NULL);
+	return (split);
+}
